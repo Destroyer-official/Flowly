bac392a27e95b1a9c91a9da25eeb7a3971dc86cc
Destroyer-official <156152706+Destroyer-official@users.noreply.github.com>
2025-12-02 00:28:54 +0530

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,12 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        getByName("debug") {
+            // Uses default debug keystore
+        }
+    }
+    
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -28,6 +34,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
